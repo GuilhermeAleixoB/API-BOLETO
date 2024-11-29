57c23359324aece0656f6958dadce6e926cbdfdc
Harpix-Guilherme <111366492+Harpix-Guilherme@users.noreply.github.com>
2024-11-29 16:30:54 -0300

    modified:   controllers/BoletoController.js

--- controllers/BoletoController.js
+++ controllers/BoletoController.js
@@ -16,14 +16,12 @@ class BoletoController {
             try {
                 QRCode.toDataURL(codigoqr, { width: 128, height: 128 }, async (err, url) => {
                     if (err) console.error(err);
-                    console.log(url);
                     const base64Data = url.split(',')[1]; 
 
                     const buffer = Buffer.from(base64Data, 'base64');
                     await banco.query(`UPDATE hpxsis.exe_boleto
                                        SET bol_st_imgqrcode = $1
                                        WHERE bol_in_sequencia = $2`, [buffer, cod]);
-                                       
                                        
                     return res.status(200).json({ status: "OK" });
            
