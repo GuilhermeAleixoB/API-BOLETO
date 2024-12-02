a59af15a4f5b30bb9c4b74acc8cc86df58bbd5ec
Harpix-Guilherme <111366492+Harpix-Guilherme@users.noreply.github.com>
2024-12-02 15:12:59 -0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const routes = require('./routes');
+const https = require('https');
+const fs = require('fs');
 require('dotenv').config();
+
+const caminhoChaveHttps = process.env.CAMINHO_CHAVE_HTTPS;
+const caminhoCertHttps = process.env.CAMINHO_CERT_HTTPS;
 
 const app = express();
 const port = process.env.PORTA;
 
-routes(app);
+if (caminhoChaveHttps == null && caminhoCertHttps == null){
+    
+    routes(app);
 
-// eslint-disable-next-line no-console
-app.listen(port, () => console.log(`servidor está rodando na porta ${port}`));
+    app.listen(port, () => console.log(`servidor está rodando na porta ${port}`));
+}else {
+    const options = {
+        key: fs.readFileSync(caminhoChaveHttps),
+        cert: fs.readFileSync(caminhoCertHttps)
+      };
+        
+    routes(app);
+    
+    https.createServer(options, app).listen(port, () => {
+        console.log(`Servidor está rodando na porta ${port}`);
+      });
+}
+
 
 module.exports = app;
